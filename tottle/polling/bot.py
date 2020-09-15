bd92ad9b46dc0484b60cef6eeaf4c5097bcad5e0
Vladislav Kovalskyi <v.darknesssb@gmail.com>
2020-09-15 22:22:22 +0300

    Update bot.py (on_startup, TaskManager)

--- tottle/polling/bot.py
+++ tottle/polling/bot.py
@@ -8,6 +8,7 @@ from tottle.api import API
 from tottle.polling.labelers.bot import BotLabeler
 from tottle.polling.polling import Polling
 from tottle.utils.logger import LoggerLevel
+from tottle.utils.taskmanager import TaskManager
 
 try:
     import uvloop
@@ -49,11 +50,13 @@ class Bot:
         logger.level("ERROR", color="<red>")
         logger.level("DEBUG", color="<white>")
 
-    def run_polling(self):
+    def run_polling(self, on_startup: typing.Callable = None):
         logger.info("Polling will be started. Is it ok?")
 
         try:
-            self.loop.create_task(self.polling.run())
-            self.loop.run_forever()
+            task = TaskManager(self.loop)
+            task.add_task(self.polling.run())
+            task.add_task(on_startup) if on_startup else None
+            task.run()
         except KeyboardInterrupt:
             logger.warning("Keyboard interrupt...")
