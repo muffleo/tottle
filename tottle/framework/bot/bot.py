ff131bbff7dbf858441dadf9a1c58559af3e9607
Михаил <Krilifongd@gmail.com>
2021-04-08 01:33:06 +0300

    Multibot fixes, now you can use it

--- tottle/framework/bot/bot.py
+++ tottle/framework/bot/bot.py
@@ -4,10 +4,10 @@ from tottle.framework.bot.labelers import BotLabeler, ABCBotLabeler
 from tottle.framework.abc import ABCFramework
 from tottle.polling import BotPolling, ABCPolling
 from tottle.dispatch.routers import BotRouter, ABCRouter
-from tottle.tools.dev_tools.loop_wrapper import LoopWrapper
+from tottle.tools.dev_tools.loop_wrapper.loop_wrapper import LoopWrapper
 from tottle.tools.dev_tools.logger import Logger, logger, default_logger
 
-from typing import Callable, Optional, NoReturn
+from typing import Callable, Optional, NoReturn, Iterable
 from asyncio import AbstractEventLoop, get_event_loop, set_event_loop_policy
 
 try:
@@ -28,24 +28,38 @@ class Bot(ABCFramework):
             loop: Optional[AbstractEventLoop] = None,
             loop_wrapper: Optional[LoopWrapper] = None,
             custom_logger: Optional[Logger] = None,
+            task_each_event: bool = False,
     ):
         self.api: "API" = API(token) if token else api
+        self.apis: Optional[Iterable["ABCAPI"]] = None
         self.loop_wrapper: "LoopWrapper" = loop_wrapper or LoopWrapper()
         self.labeler: "BotLabeler" = labeler or BotLabeler()
         self.state_dispenser = DefaultStateDispenser()
+        self.task_each_event = task_each_event
         self._router: "BotRouter" = router or BotRouter()
         self._polling: "BotPolling" = polling or BotPolling(self.api)
         self._loop = loop
 
         (custom_logger or default_logger).setup()
 
-    async def run_polling(self, custom_polling: Optional[ABCPolling] = None) -> NoReturn:
+    async def run_polling(
+            self,
+            custom_polling: Optional[ABCPolling] = None,
+            apis_count: int = 0
+    ) -> NoReturn:
         polling = custom_polling or self.polling
         logger.info("Polling will be started!")
 
         async for update in polling.listen():  # type: ignore
             logger.debug("New update: {}", update)
-            await self.router.route(update, self.api)
+
+            if self.apis:
+                self.api = self.apis[apis_count]
+
+            if not self.task_each_event:
+                await self.router.route(update, self.api)
+            else:
+                self.loop.create_task(self.router.route(update, self.api))
 
     def run_forever(self, on_startup: Optional[Callable] = None, on_shutdown: Optional[Callable] = None):
         lw = LoopWrapper(on_startup=on_startup, on_shutdown=on_shutdown)
--- tottle/framework/bot/multibot.py
+++ tottle/framework/bot/multibot.py
@@ -2,7 +2,7 @@ from typing import Iterable, Type
 from loguru import logger
 
 from tottle.api import ABCAPI
-from tottle.http import SessionManager, AiohttpClient
+from tottle.http import SingleSessionManager, AiohttpClient
 from tottle.polling import ABCPolling, BotPolling
 from .bot import Bot
 
@@ -16,6 +16,8 @@ def run_multibot(bot: Bot, apis: Iterable[ABCAPI], polling_type: Type[ABCPolling
     for i, api_instance in enumerate(apis):
         logger.debug(f"Connecting API (index: {i})")
         polling = polling_type().construct(api_instance)
-        api_instance.http = SessionManager(AiohttpClient)
-        bot.loop_wrapper.add_task(bot.run_polling(custom_polling=polling))
+        api_instance.http = SingleSessionManager(AiohttpClient)
+        bot.apis = apis
+        bot.loop_wrapper.add_task(bot.run_polling(custom_polling=polling, apis_count=i))
+
     bot.loop_wrapper.run_forever(bot.loop)
