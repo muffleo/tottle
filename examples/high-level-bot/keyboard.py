d9fa5d764121d4a33192d41c2f91ef74d6ed3100
Михаил <Krilifongd@gmail.com>
2021-04-10 15:17:27 +0300

    Updating the keyboard creation example

--- examples/high-level-bot/keyboard.py
+++ examples/high-level-bot/keyboard.py
@@ -1,28 +1,31 @@
-import os
-
 from tottle import Bot, Message
-from tottle.types.responses.keyboard import (
-    ReplyKeyboardMarkup,
-    KeyboardButton,
-    ReplyKeyboardRemove,
+from tottle.tools import Keyboard, Button, InlineKeyboard, InlineButton
+
+
+bot = Bot(token="Your token")
+
+# Creating keyboard objects
+keyboard = (
+    Keyboard(resize_keyboard=True)
+    .add(Button(text="Hello!"))
+    .row()
+    .add(Button(text="Bye!"))
+)
+inline_keyboard = (
+    InlineKeyboard()
+    .add(InlineButton(text="Tottle", url="https://github.com/tottle-project/tottle-python"))
 )
 
-bot = Bot(os.environ["TELEGRAM_TOKEN"])
-bot.labeler.vbml_ignore_case = True
 
-keyboard = ReplyKeyboardMarkup().add_buttons(
-    KeyboardButton(text="How are you?")
-)
-remove_keyboard = ReplyKeyboardRemove()
+@bot.on.message(text="Keyboard")
+async def answer(message: Message):
+    # Using the reply_markup parameter, we specify the keyboard object
+    await message.answer("Hold the keyboard!", reply_markup=keyboard.dict())
 
 
-@bot.on.message(text="hello")
-async def greet_handler(message: Message):
-    await message.answer("Hi!", reply_markup=keyboard.dict())
+@bot.on.message(text="Inline keyboard")
+async def answer(message: Message):
+    await message.answer("Hold the inline keyboard!", reply_markup=inline_keyboard.dict())
 
-
-@bot.on.message(text="how are you")
-async def question_handler(message: Message):
-    await message.answer("I'm good. Thanks for asking!", reply_markup=remove_keyboard.dict())
 
 bot.run_forever()
